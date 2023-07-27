75e2a6b21283d7bcd82df2bdc90d6fbd5ee27b19
Lautaro Decima <decimalautaro@gmail.com>
2023-07-27 13:27:40 -0300

    add: find by id

--- serverless.ts
+++ serverless.ts
@@ -1,6 +1,6 @@
 import type { AWS } from '@serverless/typescript';
 
-import hello from '@functions/hello';
+import { findAll, findById } from '@functions/index';
 
 const serverlessConfiguration: AWS = {
   service: 'serverless-curso',
@@ -19,7 +19,7 @@ const serverlessConfiguration: AWS = {
     },
   },
   // import the function via paths
-  functions: { hello },
+  functions: { findAll, findById },
   package: { individually: true },
   custom: {
     esbuild: {
---
+++ src/functions/users/findAll/handler.ts
@@ -0,0 +1,14 @@
+import { formatJSONResponse } from '@libs/api-gateway';
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import { Context } from 'vm';
+
+const hello = async (event: APIGatewayProxyEvent, context: Context) => {
+  const {name} = JSON.parse(event.body) as {name: string};
+  const {awsRequestId} = context;
+  return formatJSONResponse({
+    message: `Hello ${name}, welcome to the exciting Serverless world!`,
+    awsRequestId,
+  });
+};
+
+export const main = hello;

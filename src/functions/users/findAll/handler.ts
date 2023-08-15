91ad3d4fa1b50798b802a9fbd4c57a74f44036f0
Lautaro Decima <decimalautaro@gmail.com>
2023-08-15 00:05:50 -0300

    fix: modifying user getall and geById

--- src/functions/users/findAll/handler.ts
+++ src/functions/users/findAll/handler.ts
@@ -1,14 +1,14 @@
 import { formatJSONResponse } from '@libs/api-gateway';
-import { APIGatewayProxyEvent } from 'aws-lambda';
-import { Context } from 'vm';
 
-const main = async (event: APIGatewayProxyEvent, context: Context) => {
-  const {name} = JSON.parse(event.body) as {name: string};
-  const {awsRequestId} = context;
-  return formatJSONResponse({
-    message: `Hello ${name}, welcome to the exciting Serverless world!`,
-    awsRequestId,
-  });
+
+const main = async () => {
+  try {
+    
+  } catch (error) {
+    return formatJSONResponse({
+      error: error.message,
+    })
+  }
 };
 
 export default main ;
--- src/functions/users/findById/handler.ts
+++ src/functions/users/findById/handler.ts
@@ -1,16 +1,15 @@
 import { formatJSONResponse } from '@libs/api-gateway';
-import { APIGatewayProxyEvent } from 'aws-lambda';
-import { Context } from 'vm';
+import { APIGatewayProxyEvent, Context } from 'aws-lambda';
 
-const main = async (event: APIGatewayProxyEvent, context: Context) => {
-  // const {name} = JSON.parse(event.body) as {name: string};
-  const { id } = event.pathParameters
-  const { age } = event.queryStringParameters
-  const {awsRequestId} = context;
-  return formatJSONResponse({
-    message: `Hello ${id}, welcome to the exciting Serverless world! ${age}`,
-    awsRequestId,
-  });
+const main = async (event: APIGatewayProxyEvent, _context: Context) => {
+  try {
+    const { id } = event.pathParameters;
+  } catch (error) {
+    return formatJSONResponse({
+      error: error.message,
+    })
+  }
 };
 
 export default main;
+ 
